e88dc0b27d886ec5a6a2bcf5b02cd06c9c31fa0b
devian5 <acgarage@outlook.es>
2021-04-08 22:30:20 +0200

    Kickoff, project structure

---
+++ api/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const router = require('./router');
+const app = express();
+const cors = require('cors');
+const db = require('./db');
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+db.then(()=>{
+    app.listen(port, () => {
+        console.log('I am ready!!');
+    })
+
+}).catch(console.log);
