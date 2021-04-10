427c8f7120c299cef8871016bf87b3571898d9b2
devian5 <acgarage@outlook.es>
2021-04-10 19:23:12 +0200

    Created the method get feeling all

--- api/controllers/feeling.controller.js
+++ api/controllers/feeling.controller.js
@@ -1,9 +1,15 @@
 const Feeling = require('../models/feeling.model');
 
 class FeelingController {
+    
     async create(feeling){
         return Feeling.create(feeling);
-    }
+    };
+
+    async feelingAll(feeling){
+        return Feeling.find(feeling);
+    };
+
 };
 
 const feelingController = new FeelingController;
