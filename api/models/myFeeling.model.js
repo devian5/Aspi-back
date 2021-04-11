381a35d225bac97aac2bf652e708fb4a7418b9aa
devian5 <acgarage@outlook.es>
2021-04-11 19:11:55 +0200

    Modified my feeling model and added routes in router file

--- api/models/myFeeling.model.js
+++ api/models/myFeeling.model.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
 const myFeelingSchema = new Schema(
-    {
+    {   
+        userId: {
+            type: ObjectId
+        },
         name: {
             type: String
             
--- api/router.js
+++ api/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRouter = require('./routes/user.router');
+const feelingRouter = require('./routes/feeling.router');
+const myFeelingRouter = require('./routes/myFeeling.router');
 
 router.use('/user',userRouter);
+router.use('/feeling,',feelingRouter);
+router.use('/my-feeling',myFeelingRouter);
 
 module.exports = router;
