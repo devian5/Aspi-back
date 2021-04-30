557e151b7e16c08abc591dc9201001f21601b4bc
devian5 <acgarage@outlook.es>
2021-04-30 17:57:20 +0200

    Added route of double meaning

--- api/router.js
+++ api/router.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRouter = require('./routes/user.router');
 const feelingRouter = require('./routes/feeling.router');
 const myFeelingRouter = require('./routes/myFeeling.router');
+const doubleMeaningRouter = require('./routes/doubleMeaning.router');
 
 router.use('/user',userRouter);
-router.use('/feeling,',feelingRouter);
+router.use('/feeling',feelingRouter);
 router.use('/my-feeling',myFeelingRouter);
+router.use('/double-meaning',doubleMeaningRouter);
 
 module.exports = router;
